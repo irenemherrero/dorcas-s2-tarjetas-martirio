32daf9b5cf1770e4b776a36c7592c7156b04f406
Maria Violeta <mariavioleta1417@gmail.com>
2018-07-08 20:26:58 +0200

    details

--- js/main.js
+++ js/main.js
@@ -85,7 +85,9 @@ inputNombre.addEventListener("keyup", writeData);
 inputPuesto.addEventListener("keyup", writeData);
 
 //a parte de esto, modificar los data-id de los input y los id de los campos del preview de nombre y puesto
-
+var arrow = document.querySelector('.arrow-turn1');
+var arrowSecond = document.querySelector('.arrow-turn2');
+var arrowThird = document.querySelector('.arrow-turn3');
 var colapsables = document.querySelectorAll('.colapsable');
 
 var tituloColapsable = document.querySelectorAll('.titulo-colapsable');
@@ -94,10 +96,13 @@ function actualizarColapsable(event) {
   var contenedor = event.currentTarget.parentElement;
   
   if (contenedor.classList.contains('colapsable--visible')) {
-    contenedor.classList.remove('colapsable--visible');  
+    contenedor.classList.remove('colapsable--visible'); 
+    arrow.classList.remove('turnarrow');
+    
   } else {
     cerrarColapsables();
     contenedor.classList.add('colapsable--visible');
+    arrow.classList.add('turnarrow');
   }
 }
 
