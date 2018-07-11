ca6111dba17cdb98b750d718455f0b805d436c5d
sinsit1 <alexandra8989@gmail.com>
2018-07-11 12:35:45 +0200

    finish dinamic skills

--- js/skills.js
+++ js/skills.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /// CODIGO SKILLS //
-
+var tagsContainer = document.querySelector('.etiquetas-habilidades-container');
 var button = document.querySelector('.button');
 var divMadre = document.getElementById('mama');
 var arraySkills = [];
@@ -9,7 +9,7 @@ var selectSkills;
 var contadorClases = 1;
 var container;
 var buttonRemove;
-
+var optionAsPlaceholderText= 'Elige habilidad';
 /////////////// FETCH PARA RECOGER LOS SKILLS DEL SERVIDOR /////////////////////////////
 
 function searchArray() {
@@ -20,9 +20,12 @@ function searchArray() {
     .then(function (json) {
 
       arraySkills = json.skills;
-
+      arraySkills.push(optionAsPlaceholderText);
+      arraySkills.reverse();
       console.log(arraySkills);
     });
+
+
 
 }
 
@@ -39,20 +42,21 @@ function eliminaUltimoDiv() {
     k.parentElement.removeChild(k);
     button.classList.remove('hidden'); //Quita la clase Hidden si hay menos de 3 opciones para que aparezca el +
     contadorClases--;
-  }else {
-    alert ("Tiene que introducir alguna habilidad");
+
+    updateTagList();
+  } else {
+    alert("Tiene que introducir alguna habilidad");
   }
 }
 
 function createRemoveButton() {
   buttonRemove = document.createElement('button');
-  buttonRemove.setAttribute("type","button");
+  buttonRemove.setAttribute("type", "button");
   var insertMinus = document.createTextNode('-');
   buttonRemove.appendChild(insertMinus);
   container.appendChild(buttonRemove);
   buttonRemove.classList.add('buttonRemove');
   buttonRemove.addEventListener('click', eliminaUltimoDiv);
-
 }
 
 function changeSkills() {
@@ -71,11 +75,12 @@ function changeSkills() {
     option.appendChild(insertSkillToOption);
     // metemoslos options dentro de los select//
     selectSkills.appendChild(option);
+    selectSkills.addEventListener('change', updateTagList);
   }
 }
 
 function createDiv() {
-
+  tagsContainer.innerHTML = '';
   if (contadorClases < 3) {
     //creo un div//
     container = document.createElement('div');
@@ -90,14 +95,33 @@ function createDiv() {
     container = document.createElement('div');
     container.classList.add('container');
     divMadre.appendChild(container);
+
     changeSkills();
     createRemoveButton();
+
     contadorClases++;
-  }else {
-    alert ("Tiene que introducir alguna habilidad");
+  } else {
+    alert("Tiene que introducir alguna habilidad");
   }
 }
 
 searchArray();
-
 button.addEventListener('click', createDiv);
+
+////////////// AÑADIR A LA TARJETA ///////////////////
+
+function updateTagList() {
+  // Asigna a la variable currentListOfSelects un array con los elementos que tienen la clase ingredients__select en este momento
+  var currentListOfSelects = document.querySelectorAll('.form__select');
+
+  tagsContainer.innerHTML = ''; //limpio los skills del preview
+
+  for (var i = 0; i < currentListOfSelects.length; i++) {
+    // Asigna a la variable currentSelect el select del array currentListOfSelects correspondiente a esta vuelta del for
+    var currentSelect = currentListOfSelects[i];
+    // Si currentSelect no tiene seleccionada la opción que hace de placeholder, añade un nuevo li al listado de tags
+    if(currentSelect.value !== optionAsPlaceholderText) {
+      tagsContainer.innerHTML += '<li class="etiqueta-habilidad">' + currentSelect.value + '</li>';
+    }
+  }
+}
